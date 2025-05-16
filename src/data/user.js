e2fb3752120aa7f1f95d2d73632701e9694f5bc2
summer077 <msnbaldonado@tip.edu.ph>
2025-05-17 00:45:19 +0800

    initialized

---
+++ src/data/user.js
@@ -0,0 +1,43 @@
+const INFO = {
+	main: {
+		title: "Portfolio",
+		name: "Stephen Nash B. Baldonado",
+		email: "msnbaldonado@tip.edu.ph",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Summer077",
+		linkedin: "https://www.linkedin.com/in/stephen-nash-baldonado-71610434b/",
+		instagram: "https://www.instagram.com/sumxr_07/",
+		facebook: "https://www.facebook.com/stephennash09/",
+	},
+
+	homepage: {
+		title: "Full-time Student and Amateur Developer.",
+		description:
+			"I'm a Full-time Student and an Amateur Developer pursuing a degree in Computer Engineering. Passionate about technology, I enjoy exploring software development, hardware systems, and problem-solving. Constantly learning and improving, I work on personal projects, coding challenges, and new technologies to enhance my skills. My goal is to bridge the gap between hardware and software, creating innovative and efficient solutions.",
+	},
+
+	about: {
+		title: "I’m Stephen Nash B. Baldonado. I live in Manila, where I develop website.",
+		description:
+			"I’m Stephen Nash B. Baldonado, a full-time student and an amateur developer pursuing Computer Engineering. Based in Manila, I specialize in web development, crafting intuitive and engaging digital experiences. Constantly exploring new technologies, I strive to blend creativity with technical expertise to build innovative projects.",
+	},
+
+	projects: [
+		{
+			title: "Portfolio Website",
+			description:
+				"A simple portfolio website for a cat-inspired coffee shop. It showcases the shop’s cozy vibe, menu highlights, and playful cat-themed design in a clean, user-friendly layout.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/AVINGIRS/AVINGIRS-ASIMBOL?tab=readme-ov-file#avingirs-asimbol",
+			
+		},
+
+		
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							What I've Accomplished.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+						After several months of dedicated learning and hands-on practice, 
+						I have successfully built a project that reflects my progress and 
+						growing skills. By leveraging an open-source repository, I aim to 
+						share my work with others and encourage collaboration. I'm always 
+						open to feedback, suggestions, and ideas for improvement to further 
+						enhance the project and continue learning from the community.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
